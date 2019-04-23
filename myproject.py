94d09806b5d7f199ad3d295354c1b959658f3079
Mujtaba33 <49758355+Mujtaba33@users.noreply.github.com>
2019-04-23 19:41:08 +0500

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -7,3 +7,29 @@ for i in range(1,6):
 		else:
 			print("5",end=" ")
 	print("")
+	
+	
+	
+	
+		
+rowsize=int(input("enter no of rows"))
+for row in range(rowsize+1):
+	for space in range(rowsize,row,-1):
+		print(" ",end=" ")
+	colsize=((row*2)-1)
+	for col in range(colsize):
+		if(col%2==0):
+			print("*",end=" ")
+		else:
+			print("6",end=" ")
+	print()
+for row in range(rowsize-1,0,-1):
+	for space in range(rowsize,row,-1):
+		print(" ",end=" ")
+	colsize=((row*2)-1)
+	for col in range(colsize):
+		if(col%2):
+			print("6",end=" ")
+		else:
+			print("*",end=" ")
+	print()
