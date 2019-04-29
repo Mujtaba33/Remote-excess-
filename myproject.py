e48b68eef1533fa84d490c47a3e01f68753fe736
Mujtaba33 <49758355+Mujtaba33@users.noreply.github.com>
2019-04-29 19:59:44 +0500

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,35 +1,6 @@
-for i in range(1,6):
-	for j in range (i,6):
-		print(" ",end=" ")
-	for k in range(1,i*2):
-		if(k%2==0):
-			print("7",end=" ")
-		else:
-			print("5",end=" ")
-	print("")
-	
-	
-	
-	
-		
-rowsize=int(input("enter no of rows"))
-for row in range(rowsize+1):
-	for space in range(rowsize,row,-1):
-		print(" ",end=" ")
-	colsize=((row*2)-1)
-	for col in range(colsize):
-		if(col%2==0):
-			print("*",end=" ")
-		else:
-			print("6",end=" ")
-	print()
-for row in range(rowsize-1,0,-1):
-	for space in range(rowsize,row,-1):
-		print(" ",end=" ")
-	colsize=((row*2)-1)
-	for col in range(colsize):
-		if(col%2):
-			print("6",end=" ")
-		else:
-			print("*",end=" ")
-	print()
+>>> import pyautogui
+>>> for i in range(10):
+      pyautogui.moveRel(100, 0, duration=0.25)
+      pyautogui.moveRel(0, 100, duration=0.25)
+      pyautogui.moveRel(-100, 0, duration=0.25)
+      pyautogui.moveRel(0, -100, duration=0.25)
