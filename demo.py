9b59f928492c25f388bdd86272e0b7369cff386f
AMEENRANA <49764264+AMEENRANA@users.noreply.github.com>
2019-05-01 14:57:30 +0500

    This is demo

---
+++ demo.py
@@ -0,0 +1,6 @@
+def behaviour2() :
+    pyautogui.click (500, 200)
+    pyautogui.dragRel (100, 500, duration = 0.27)
+    pyautogui.doubleclick()
+    pyautogui.scroll (600)
+    pyautogui.rightclick() 
